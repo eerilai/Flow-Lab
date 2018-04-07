594e04c7554e003815290096f1339dc2381e5de9
eerilai <eerilaie@gmail.com>
2018-04-06 19:38:29 -0500

    Import DB and provide helper query functions

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const db = require('./database/index.js');
 
 
 const app = express();
 app.use(express.static(path.join(__dirname, './client/dist')));
 
-app.get('/', (req, res) => res.send('Hello World'));
+// app.get('/', (req, res) => res.send('Hello World'));
 
 app.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`));
