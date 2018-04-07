4d33b2d2ca02bc2068645c0042f7de17c5eb003f
eerilai <eerilaie@gmail.com>
2018-04-06 19:00:12 -0500

    Create initial Schema for DB

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE flow_trainer; 
+
+\c flow_trainer;
+
+CREATE TABLE arts (
+  art_id integer PRIMARY KEY, 
+  name varchar(32) UNIQUE NOT NULL
+);
+
+CREATE TABLE modes(
+  mode_id integer NOT NULL PRIMARY KEY,
+  name varchar(32) UNIQUE NOT NULL
+);
+
+CREATE TABLE arts_modes(
+  art_id integer REFERENCES arts ON DELETE CASCADE,
+  mode_id integer REFERENCES modes ON DELETE CASCADE,
+  PRIMARY KEY (art_id, mode_id)
+);
