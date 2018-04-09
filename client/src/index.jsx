382f4d63f29896d246c3ac14615ba76342cd74f6
eerilai <eerilaie@gmail.com>
2018-04-09 02:12:08 -0500

    Take input for minutes and seconds

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,11 +7,15 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.getModes = this.changeArt.bind(this);
+
     this.state = {
       arts: [],
       modes: [],
       activeArt: '',
       activeModes: [],
+      time: 0,
+      min: 0,
+      sec: 0,
     };
   }
 
@@ -24,6 +28,20 @@ class App extends React.Component {
     const data = await axios.get('/data');
     this.setState({ arts: data.data });
     console.log(this.state.arts);
+  }
+
+  async setSec(val) {
+    let secs = this.state.sec;
+    secs = val;
+    await this.setState({ sec: secs });
+    this.updateTime();
+  }
+
+  async setMin(val) {
+    let mins = this.state.min;
+    mins = val;
+    await this.setState({ min: mins });
+    this.updateTime();
   }
 
   async changeArt({ art }) {
@@ -39,7 +57,7 @@ class App extends React.Component {
   toggleMode({ mode }) {
     if (this.state.activeModes.indexOf(mode) !== -1) {
       const removeMode = this.state.activeModes;
-      removeMode.splice(removeMode.indexOf(mode), 0);
+      removeMode.splice(removeMode.indexOf(mode), 1);
       this.setState({ activeModes: removeMode });
     } else {
       const addMode = this.state.activeModes;
@@ -47,6 +65,12 @@ class App extends React.Component {
       this.setState({ activeModes: addMode });
     }
     console.log('active modes: ', this.state.activeModes);
+  }
+
+  updateTime() {
+    const time = (this.state.min * 60) + this.state.sec;
+    this.setState({ time });
+    console.log(this.state.time);
   }
 
   render() {
@@ -64,6 +88,10 @@ class App extends React.Component {
             <span>{mode}
               <input type="checkbox" checked={this.state.activeModes.indexOf(mode) !== -1} className="modeSelect" onChange={() => this.toggleMode({ mode })} />&nbsp;
             </span>))}
+        </div>
+        <div>
+          <span><input type="number" name="minutes" value={this.state.min} min="0"onChange={e => this.setMin(e.target.value)} required />m</span>
+          <span><input type="number" name="seconds" value={this.state.sec} max="59" min="0" onChange={e => this.setSec(e.target.value)}required />s</span>
         </div>
       </div>
     );
