acf8beb15ba1b7996c7ddff0b2bf835150e72ef5
eerilai <eerilaie@gmail.com>
2018-04-09 00:57:26 -0500

    Implemented input fields for selecting art and modes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.getModes = this.changeArt.bind(this);
     this.state = {
+      arts: [],
+      modes: [],
+      activeArt: '',
+      activeModes: [],
     };
   }
 
   componentDidMount() {
+    this.getArts();
+  }
 
+  async getArts() {
+    console.log('getting arts!');
+    const data = await axios.get('/data');
+    this.setState({ arts: data.data });
+    console.log(this.state.arts);
+  }
+
+  async changeArt({ art }) {
+    console.log(art);
+    console.log('changing arts!');
+    this.setState({ activeModes: [] });
+    const data = await axios.get(`/data?art=${art}`);
+    this.setState({ modes: data.data });
+    console.log('active modes: ', this.state.activeModes);
+    console.log('new modes!:', this.state.modes);
+  }
+
+  toggleMode({ mode }) {
+    if (this.state.activeModes.indexOf(mode) !== -1) {
+      const removeMode = this.state.activeModes;
+      removeMode.splice(removeMode.indexOf(mode), 0);
+      this.setState({ activeModes: removeMode });
+    } else {
+      const addMode = this.state.activeModes;
+      addMode.push(mode);
+      this.setState({ activeModes: addMode });
+    }
+    console.log('active modes: ', this.state.activeModes);
   }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <div>
+          <h1>Select your Flow!</h1>
+          {this.state.arts.map(art => (
+            <span className="art">{art}
+              <input type="radio" name="artSelect" value={art} onChange={() => this.changeArt({ art })} />&nbsp;
+            </span>))}
+        </div>
+        <div>
+          {this.state.modes.map(mode => (
+            <span>{mode}
+              <input type="checkbox" checked={this.state.activeModes.indexOf(mode) !== -1} className="modeSelect" onChange={() => this.toggleMode({ mode })} />&nbsp;
+            </span>))}
+        </div>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -15,34 +15,37 @@ app.get('/', (req, res) => {
 
 app.get('/data', async (req, res) => {
   console.log('getting data');
-  const data = await db('arts')
-    .innerJoin('art_mode', 'arts.id', 'art_mode.art_id')
-    .innerJoin('modes', 'art_mode.mode_id', 'modes.id')
-    .then(data => console.log(data));
-  // console.log(data);
-  res.end();
+  if (req.query.art) {
+    res.json(await db('arts').select('mode')
+      .innerJoin('art_mode', 'arts.id', 'art_mode.art_id')
+      .innerJoin('modes', 'art_mode.mode_id', 'modes.id')
+      .where('art', req.query.art)
+      .map(x => x.mode));
+  } else {
+    res.json(await db('arts').select('art').map(x => x.art));
+  }
 });
 
 app.post('/addart', async (req, res) => {
-  await db('arts').insert({ name: req.body.art });
+  await db('arts').insert({ art: req.body.art });
   res.end();
 });
 
 app.post('/addmode', async (req, res) => {
   console.log(req.body);
   try {
-    let mId = await db('modes').select('id').where('name', req.body.mode);
-    let aId = await db('arts').select('id').where('name', req.body.art);
+    let mId = await db('modes').select('id').where('mode', req.body.mode);
+    let aId = await db('arts').select('id').where('art', req.body.art);
     console.log(mId);
     if (!mId.length) {
-      await db('modes').insert({ name: req.body.mode });
-      mId = await db('modes').select('id').where('name', req.body.mode);
+      await db('modes').insert({ mode: req.body.mode });
+      mId = await db('modes').select('id').where('mode', req.body.mode);
     }
     mId = mId[0].id;
     aId = aId[0].id;
     console.log(mId, aId);
-    await db('art_mode').insert({ art_id: aId, mode_id: mId }).whereNotExists(function() {
-      db('art_mode').select('*').where({ art_id:aId, mode_id:mId });
+    await db('art_mode').insert({ art_id: aId, mode_id: mId }).whereNotExists(() => {
+      db('art_mode').select('*').where({ art_id: aId, mode_id: mId });
     });
   } catch (error) {
     console.error(error);
