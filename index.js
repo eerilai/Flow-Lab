f10235f58185544d9f3f0bf4b1149ff08498ee5a
eerilai <eerilaie@gmail.com>
2018-07-20 17:16:21 -0500

    Remove legacy database setup and API

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const db = require('./database/config.js');
 const bodyParser = require('body-parser');
 
 
@@ -11,55 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.end();
-});
-
-app.post('/', async (req, res) => {
-  console.log(req.body);
-  if (req.body.mode.length === 0) {
-    await db('arts').insert({ art: req.body.art });
-    res.end();
-  }
-  res.end();
-});
-
-app.get('/data', async (req, res) => {
-  console.log('getting data');
-  if (req.query.art) {
-    res.json(await db('arts').select('mode')
-      .innerJoin('art_mode', 'arts.id', 'art_mode.art_id')
-      .innerJoin('modes', 'art_mode.mode_id', 'modes.id')
-      .where('art', req.query.art)
-      .map(x => x.mode));
-  } else {
-    res.json(await db('arts').select('art').map(x => x.art));
-  }
-});
-
-app.post('/addart', async (req, res) => {
-  await db('arts').insert({ art: req.body.art });
-  res.end();
-});
-
-app.post('/addmode', async (req, res) => {
-  console.log(req.body);
-  try {
-    let mId = await db('modes').select('id').where('mode', req.body.mode);
-    let aId = await db('arts').select('id').where('art', req.body.art);
-    console.log(mId);
-    if (!mId.length) {
-      await db('modes').insert({ mode: req.body.mode });
-      mId = await db('modes').select('id').where('mode', req.body.mode);
-    }
-    mId = mId[0].id;
-    aId = aId[0].id;
-    console.log(mId, aId);
-    await db('art_mode').insert({ art_id: aId, mode_id: mId }).whereNotExists(() => {
-      db('art_mode').select('*').where({ art_id: aId, mode_id: mId });
-    });
-  } catch (error) {
-    console.error(error);
-  }
   res.end();
 });
 
